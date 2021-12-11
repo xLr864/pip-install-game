bb3a2077a999388bc2a3a5d71034fd4c23129771
Chetan <78348378+xLr864@users.noreply.github.com>
2021-12-12 00:25:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(name="Games",version="1.0",
 description="This package will randomly recommand a game to play",
 author="Chetan",
-packages=['game'],
+packages=['Games'],
 )
