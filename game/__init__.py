76faae304445412b1c0bb899923e0b9ffb6cb0e4
xLr864 <2018pietcschetan42@poornima.org>
2021-12-11 22:59:03 +0530

    First commit

---
+++ game/__init__.py
@@ -0,0 +1,11 @@
+import random
+def khelo():
+    i = random.randint(1,100)
+    if(i>=1 and i<=24):
+        return("Grand Theft Auto 5")
+    elif(i>=25 and i<=49):
+        return("Red Dead Redemption 2")
+    elif(i>=50 and i<=74):
+        return("GTA Trilogy: The Definitive Edition")
+    if(i>=75 and i<=100):
+        return("Resident Evil 4")
